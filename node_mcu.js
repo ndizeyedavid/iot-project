01f65d0dab39cd20f9c02c9be95e4114027989ea
David Ndizeye <126089767+ndizeyedavid@users.noreply.github.com>
2024-11-02 06:11:29 -0700

    fixed cors issue

--- node_mcu.js
+++ node_mcu.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mysql = require("mysql2");
+const cors = require("cors");
 const port = 8080;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => {
   console.log("Server is running...");
